2e07a005cf243444170f3d6cbf1711055295e15c
Isha-prog <rekhas.singh2004@gmail.com>
2026-02-08 19:42:41 +0530

    Function interface with different parameters

--- Day1_functioninterface.java
+++ Day1_functioninterface.java
@@ -1,15 +1,26 @@
-@FunctionalInterface
-interface  hello {
-void display();
+// @FunctionalInterface
+// interface  hello {
+// void display();
     
+// }
+// public class Day1_functioninterface{
+//     public static void main (String args[]){
+
+//         //using lambda function of zero parameter 
+
+// hello h=()->System.out.println("hello");
+// h.display();
+//     }
+// }
+
+//with parameter
+interface hii{
+    int display(int a,int b);
 }
 public class Day1_functioninterface{
     public static void main (String args[]){
-
-        //using lambda function
-
-hello h=()->System.out.println("hello");
-h.display();
+        hii h =(a,b)-> a+b; ;
+        int result=h.display(10,20);
+     System.out.println(result);
     }
 }
-
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+// public class Day1_functioninterface{
+//     public static void main (String args[]){
+
+//         //using lambda function of zero parameter 
+
+// hello h=()->System.out.println("hello");
+// h.display();
+//     }
+// }
